95f801f4acb464f25bf0df2385cecbfbb6d2ce7c
Gracielena Gonzalez <ggonz456@fiu.edu>
2022-02-17 21:38:40 -0500

    adding SelfCheckCapable Iterface to Microphone

--- src/main/java/ggtclasse/Microphone.java
+++ src/main/java/ggtclasse/Microphone.java
@@ -3,10 +3,25 @@
  */
 package ggtclasse;
 
+import edu.fiu.jit.SelfCheckCapable;
+import edu.fiu.jit.SelfCheckUtils;
+
 /**
  * @author Gracielena_home
  *
  */
-public class Microphone extends ListenElements{
+public class Microphone extends ListenElements implements SelfCheckCapable{
+
+	@Override
+	public String getComponentName() {
+		// TODO Auto-generated method stub
+		return "Microphone";
+	}
+
+	@Override
+	public boolean selfCheck() {
+		// TODO Auto-generated method stub
+		return SelfCheckUtils.randomCheck(0.8);
+	}
 
 }
